9b8b74305e323e0b27ff273c7c4573decf967e39
Nick Pazoles <npazo@users.noreply.github.com>
2015-06-26 14:44:06 -0500

    Create windows.bat

---
+++ windows.bat
@@ -0,0 +1,5 @@
+xcopy
+/s   : Copies directories and subdirectories, unless they are empty. If you omit /s, xcopy works within a single directory. 
+/e   : Copies all subdirectories, even if they are empty. Use /e with the /s and /t command-line options. 
+/y   : Suppresses prompting to confirm that you want to overwrite an existing destination file. 
+/i   : If Source is a directory or contains wildcards and Destination does not exist, xcopy assumes destination specifies a directory name and creates a new directory. Then, xcopy copies all specified files into the new directory. By default, xcopy prompts you to specify whether Destination is a file or a directory
