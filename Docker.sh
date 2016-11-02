ee06310a153cc8355234f5a5519ec03178de3beb
Nick Pazoles <npazo@users.noreply.github.com>
2016-11-02 07:37:51 -0500

    Update Docker.sh

--- Docker.sh
+++ Docker.sh
@@ -4,3 +4,12 @@ docker rm $(docker ps -q -f status=exited)
 
 ## Remove all untagged docker images
 docker images | grep "<none>" | awk '{print $3}' | xargs docker rmi
+
+# Delete all containers
+docker rm $(docker ps -a -q)
+
+# Delete all images
+docker rmi $(docker images -q)
+
+# Delete dangling volumes
+docker volume rm $(docker volume ls -qf dangling=true)
