151e85945e19fefeaf2bd633ad16a50d33ce497f
Nick Pazoles <npazo@users.noreply.github.com>
2017-01-10 11:08:23 -0600

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -6,3 +6,15 @@ tar -zcvf archive-name.tar.gz directory-name
 
 # untar
 tar -zxvf archive-name.tar.gz
+
+# save all added/modified files between the current branch and the main branch. Like a stash but for PRs sort of
+git_save {
+  tmpDir="./git.diff.files"
+  gitRoot=`git rev-parse --show-toplevel`
+  mainBranch="develop"
+  
+  mkdir $tmpDir
+  git diff --name-only --diff-filter=AM $mainBranch | while read -r line || [[ -n "$line" ]]; do
+   ditto $gitRoot/$line $tmpDir/$line
+  done
+}
