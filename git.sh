cab4bdc1220cd3d83cd7172e059e6325f8903474
Nick Pazoles <npazo@users.noreply.github.com>
2017-03-03 10:17:07 -0600

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,2 +1,5 @@
 # Has a feature branch ever been merged
 git log feature_branch master --oneline --date-order --merges --reverse -1
+
+# create new branch and switch to it
+git checkout -b newbranch
