0e401d6eb271f33366793aef7947aa585abb50d8
Nick Pazoles <npazo@users.noreply.github.com>
2017-05-03 15:14:52 -0500

    Update git.sh

--- git.sh
+++ git.sh
@@ -6,3 +6,7 @@ git checkout -b newbranch
 
 # stages modified and deleted, without new
 git add -u
+
+# --cached means show the changes in 
+#   the cache/index (i.e. staged changes) against the current HEAD. --staged is a synonym for --cached.
+git diff --cached
