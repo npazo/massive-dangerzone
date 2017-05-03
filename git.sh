134da11dc3e50701c8ddd2746f886c32dd1a2ebf
Nick Pazoles <npazo@users.noreply.github.com>
2017-05-03 15:12:47 -0500

    Update git.sh

--- git.sh
+++ git.sh
@@ -3,3 +3,6 @@ git log feature_branch master --oneline --date-order --merges --reverse -1
 
 # create new branch and switch to it
 git checkout -b newbranch
+
+# stages modified and deleted, without new
+git add -u
