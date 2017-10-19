af37ba2e68ec7da223440e872e0891db8d7ae0d8
Nick Pazoles <npazo@users.noreply.github.com>
2017-10-19 13:00:48 -0500

    Add group add

--- linux.sh
+++ linux.sh
@@ -1,6 +1,9 @@
 # add a new user; with a group of the same name (-U) with a specific UID (-u), shell (-s) and home dir (-d)
 sudo useradd -U -u 1902 -s /bin/bash -d /var/lib/jenkins [USERNAME]
 
+# add user to group
+usermod -a -G {group-name} username
+
 # tar directory
 tar -zcvf archive-name.tar.gz directory-name
 
