9cce15475f19582b0b8a65e60d541e93f8933c5e
Nick Pazoles <npazo@users.noreply.github.com>
2019-08-06 09:49:12 -0500

    Update postgresql.sql

--- postgresql.sql
+++ postgresql.sql
@@ -4,3 +4,6 @@ SELECT sum(n_live_tup)
   FROM pg_stat_user_tables;
 
 psql -h <HOSTNAME> -U <USERNAME> -d <DATABASE>
+
+postgres=# create user myuser with encrypted password 'mypass';
+postgres=# grant all privileges on database mydb to myuser;
