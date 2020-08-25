8c25800694ab5756e36f1ed3729fa6cb3c5cbae9
Nick Pazoles <npazo@users.noreply.github.com>
2020-08-25 09:11:28 -0500

    Update postgresql.sql

--- postgresql.sql
+++ postgresql.sql
@@ -9,3 +9,6 @@ postgres=# create user myuser with encrypted password 'mypass';
 postgres=# grant all privileges on database mydb to myuser;
 
 psql -h <HOSTNAME> -U <USER> -d <DATABASE> -a -f listpgp.sql
+
+# dump only specific schema to a file
+pg_dump -n <SCHEMA_NAME> > output.sql
